dd9771d66858706ed0d3b931d4a9e36a65cf92bc
Michal-W-dev <maikey20@gmail.com>
2022-07-01 22:35:30 +0200

    Product page - fix switching product pages from mini-cart.

--- src/app/product/feature/product.component.ts
+++ src/app/product/feature/product.component.ts
@@ -11,14 +11,16 @@ import productsSeed from 'src/seed'
 export class ProductComponent {
   imageIdx = 0;
   qty = 1;
-  id = this.route.snapshot.params['id']
-  product: IProduct = productsSeed.find(({ _id }) => _id === this.id) || productsSeed[0]
+  id?: string;
+  product!: IProduct;
 
   constructor(private route: ActivatedRoute, private router: Router) {
+    route.params.subscribe(({ id }) => [this.id, this.product] = [id, this.getProduct(id)])
   }
 
   handleAddToCart = () => this.router.navigateByUrl(`/cart/${this.id}?qty=${this.qty}`)
 
   get numReviews() { return this.product.numReviews === 1 ? '1 review' : `${this.product.numReviews} reviews` }
   get qtyArray() { return [...Array(this.product.countInStock > 10 ? 10 : this.product.countInStock)] }
+  getProduct = (id: string) => productsSeed.find(({ _id }) => _id === id) || productsSeed[0]
 }
